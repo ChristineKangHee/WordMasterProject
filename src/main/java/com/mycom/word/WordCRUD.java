96259f40d211615861dabaddc14c87086576ffa4
ChristineKangHee <ChristineKangHee@gmail.com>
2022-09-17 15:26:34 +0900

    Update WordMaster Project1

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -166,7 +166,7 @@ public class WordCRUD implements ICRUD{
         listAll(level);
     }
     public void searchWord() {
-        System.out.print("=> 원하는 단어은? ");
+        System.out.print("=> 원하는 단어는? ");
         String keyword=s.next();
         listAll(keyword);
     }
